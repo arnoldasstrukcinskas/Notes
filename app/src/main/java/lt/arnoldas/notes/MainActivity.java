266c92f3bd98fec99fc766bdebba781e5cba7b60
arnoldasstrukcinskas <141561525+arnoldasstrukcinskas@users.noreply.github.com>
2023-08-29 21:23:57 +0300

    Implement ListView in MainActivity

--- app/src/main/java/lt/arnoldas/notes/MainActivity.java
+++ app/src/main/java/lt/arnoldas/notes/MainActivity.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import lt.arnoldas.notes.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +25,30 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.myTextView.setText("Kokia grazi diena ir koks as nuostabus");
-        binding.myTextView.setTextSize(55);
-        binding.myTextView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        ArrayList<String> list = new ArrayList<>();
+
+        List<String> newList = Arrays.asList(
+                "Pirmadienis",
+                "Antradienis",
+                "Treciadienis",
+                "Ketvirtadienis",
+                "Penktadienis",
+                "Sestadienis",
+                "Sekmadienis"
+        );
+
+        list.addAll(newList);
+        list.addAll(newList);
+        list.addAll(newList);
+        list.addAll(newList);
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+
+        binding.notesListView.setAdapter(adapter);
+
+        //PAVYZDYS
+//        binding.myTextView.setTextSize(55);
+//        binding.myTextView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
 
 //        TextView myTextView = findViewById(R.id.myTextView);          // textview geriau nenadoti, geresnis binding, nes su juo aplikacija veikia greiciau ir stabiliau
 //        myTextView.setText("Kokia grazi diena ir koks as nuostabus");
